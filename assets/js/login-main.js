3f35c40d361a41123d1f68705a0209b35a423b31
Gaurav Jain <crazcuber@gmail.com>
2017-11-05 11:16:13 +0530

    Auth
    Added auth and fixed firebase errors

---
+++ assets/js/login-main.js
@@ -0,0 +1,39 @@
+
+$("#signIn").click(function sign(){
+    /*
+    var email = $("#inputEmail").val();
+    var password = $("#inputPassword").val();
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("error code "+errorCode+" errorMessage "+errorMessage);
+  // ...
+    if(error===null){
+        $(location).attr('href',"profile.html");
+    }
+});*/
+    
+    console.log("clck");
+    var provider =  new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;  
+        // ...
+        console.log(user);
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        
+        console.log("error"+errorMessage);
+    });
+});
